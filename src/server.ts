4960f968edeb559006b14d746f4d2a4b7273d20a
safad643 <safad643@gmail.com>
2026-06-12 14:33:34 +0530

    wastreee

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 
-import Fastify from 'fastif';
+import Fastify from 'fasti';
 import { TypeBoxTypeProvider } from '@fastify/type-provider-typebox';
 import helmet from '@fastify/helmet';
 import cors from '@fastify/cors';
